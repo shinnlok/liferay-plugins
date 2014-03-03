3e9b751ca2b818a5ccb3109a19ab72a3a348b03c
Dennis Ju <dennis.ju@liferay.com>
2014-03-03 14:38:27 -0800

    My test DownloadFileEventTest (just currently a copy of GetAllSyncDLObjectsEventTest)

---
+++ shared/sync-engine-shared/test/unit/com/liferay/sync/engine/documentlibrary/event/DownloadFileEventTest.java
@@ -0,0 +1,126 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sync.engine.documentlibrary.event;
+
+import com.liferay.sync.engine.BaseTestCase;
+import com.liferay.sync.engine.http.Session;
+import com.liferay.sync.engine.model.SyncFile;
+import com.liferay.sync.engine.model.SyncSite;
+import com.liferay.sync.engine.service.SyncFileService;
+import com.liferay.sync.engine.service.SyncSiteService;
+import com.liferay.sync.engine.util.FileUtil;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.client.ResponseHandler;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mockito;
+
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * @author Shinn Lok
+ * @author Dennis Ju
+ */
+
+@PowerMockIgnore("javax.net.ssl.*")
+@RunWith(PowerMockRunner.class)
+public class DownloadFileEventTest extends BaseTestCase {
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_syncSite = SyncSiteService.addSyncSite(
+			10158, filePathName + "/test-site", 10184,
+			syncAccount.getSyncAccountId());
+
+		SyncFileService.addSyncFile(
+			null, null, null,
+			FileUtil.getFileKey(syncAccount.getFilePathName()),
+			syncAccount.getFilePathName(), null, "test", 0,
+			_syncSite.getGroupId(), syncAccount.getSyncAccountId(),
+			SyncFile.TYPE_FOLDER);
+	}
+
+	@After
+	@Override
+	public void tearDown() throws Exception {
+		super.tearDown();
+
+		SyncSiteService.deleteSyncSite(_syncSite.getSyncSiteId());
+
+		for (SyncFile syncFile : _syncFiles) {
+			SyncFileService.deleteSyncFile(syncFile.getSyncFileId());
+		}
+	}
+
+	@Test
+	public void testRun() throws Exception {
+//		setGetResponse("dependencies/get_all_sync_dl_objects.json");
+//		setPostResponse("dependencies/get_all_sync_dl_objects.json");
+
+		Map<String, Object> parameters = new HashMap<String, Object>();
+
+		parameters.put("folderId", 0);
+		parameters.put("repositoryId", _syncSite.getGroupId());
+
+		GetAllSyncDLObjectsEvent getAllSyncDLObjectsEvent =
+			new GetAllSyncDLObjectsEvent(
+				syncAccount.getSyncAccountId(), parameters);
+
+		String response = readResponse(
+			"dependencies/get_all_sync_dl_objects.json");
+
+		Mockito.mock(Session.class);
+
+		Mockito.when(
+			getAllSyncDLObjectsEvent.getSession().executePost(
+				Mockito.anyString(), Mockito.anyMap(),
+				Mockito.any(ResponseHandler.class))
+		).thenReturn(
+			response
+		);
+
+		getAllSyncDLObjectsEvent.run();
+
+		_syncFiles = SyncFileService.findSyncFiles(
+			syncAccount.getSyncAccountId());
+
+		Assert.assertEquals(4, _syncFiles.size());
+
+		Path filePath = Paths.get(filePathName + "/Document_1.txt");
+
+		Assert.assertTrue(Files.exists(filePath));
+	}
+
+	private List<SyncFile> _syncFiles;
+	private SyncSite _syncSite;
+
+}
