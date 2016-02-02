ebb5a3d972a87346aba4b3eeb9df8593148f5419
Dennis Ju <dennis.ju@liferay.com>
2016-02-01 16:12:28 -0800

    SYNC-1539 SF

--- webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/servlet/DownloadServlet.java
+++ webs/sync-web/docroot/WEB-INF/src/com/liferay/sync/servlet/DownloadServlet.java
@@ -484,8 +484,9 @@ public class DownloadServlet extends HttpServlet {
 				}
 			}
 			catch (Exception e) {
-				processException(
-					zipFileId, e.getClass().getName(), errorsJSONObject);
+				Class clazz = e.getClass();
+
+				processException(zipFileId, clazz.getName(), errorsJSONObject);
 			}
 		}
 
